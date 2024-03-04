262d7620391c1dc354e3be9b897b0da2790e239f
Troley <troleyhs@gmail.com>
2024-03-04 16:49:14 +0100

    Fixed venv and add executable file

--- gui.py
+++ gui.py
@@ -1,19 +1,25 @@
+import os
 import time
 
 import PySimpleGUI as sg
 
 import functions
 
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", "w") as file:
+        pass
+
 sg.theme('DarkBlack')
 
 clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip='Enter todo', key="todo")
-add_button = sg.Button(size=2, image_source="add.png", key='Add', mouseover_colors="LightBlue2", tooltip="Add Todo")
+add_button = sg.Button(size=2, image_source="files/add.png", key='Add', mouseover_colors="LightBlue2",
+                       tooltip="Add Todo")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
-complete_button = sg.Button(size=10, image_source="complete.png", key='Complete')
+complete_button = sg.Button(size=10, image_source="files/complete.png", key='Complete')
 
 exit_button = sg.Button("Exit")
 
