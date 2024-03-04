dc5e73422d862b03892e7ab77ffef082284476dc
Troley <troleyhs@gmail.com>
2024-03-04 10:54:26 +0100

    Add add and complete buttons

--- gui.py
+++ gui.py
@@ -9,11 +9,11 @@ sg.theme('DarkBlack')
 clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip='Enter todo', key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button(size=2, image_source="add.png", key='Add', mouseover_colors="LightBlue2", tooltip="Add Todo")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
+complete_button = sg.Button(size=10, image_source="complete.png", key='Complete')
 
 exit_button = sg.Button("Exit")
 
@@ -27,10 +27,10 @@ window = sg.Window('My To-Do App',
 
 while True:
     event, values = window.read(timeout=200)
-    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+    if event == sg.WINDOW_CLOSED:
+        break
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))  # Simple clock
     print(1, event)
-    print(2, values)
-    print(3, values['todos'])
     match event:
         case "Add":
             todos = functions.get_todos()  # Get existing todos from file
@@ -64,7 +64,5 @@ while True:
             break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
-        case sg.WINDOW_CLOSED:  # Closes program on 'x'
-            break
 
 window.close()
